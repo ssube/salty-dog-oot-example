be453d3e66401ef9a37a14719f620b4d96dd6c7f
ssube <seansube@gmail.com>
2019-11-02 11:24:57 -0500

    fix(rules): group errors

--- src/rule.js
+++ src/rule.js
@@ -28,16 +28,13 @@ class FailingRule {
   }
 
   async visit(ctx, node) {
-    // stream errors directly to the context
-    ctx.error({
-      data: {},
-      level: 'error',
-      msg: 'oot-error',
-    });
-
     return {
       changes: [],
-      errors: [/* or pass them in a group at the end */],
+      errors: [{
+        data: {},
+        level: 'error',
+        msg: 'oot-error',
+      }],
     };
   }
 }
